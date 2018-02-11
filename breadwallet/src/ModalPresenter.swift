0b69c677804d8cfb6cb32a42b426ce94d97defef
AdelaideCreative <david@adelaidecreative.com.au>
2018-02-12 00:40:01 +1030

    Removed "Share Anonymous Data" setting

--- breadwallet/src/ModalPresenter.swift
+++ breadwallet/src/ModalPresenter.swift
@@ -404,12 +404,12 @@ class ModalPresenter : Subscriber, Trackable {
                 })
             ],
             "StrayaWallet": [
-                Setting(title: S.Settings.shareData, callback: {
-                    settingsNav.pushViewController(ShareDataViewController(store: self.store), animated: true)
-                }),
+                //Setting(title: S.Settings.shareData, callback: {
+                //    settingsNav.pushViewController(ShareDataViewController(store: self.store), animated: true)
+                //}),
                 Setting(title: S.Settings.about, callback: {
                     settingsNav.pushViewController(AboutViewController(), animated: true)
-                }),
+                })
             ],
             "Advanced": [
                 Setting(title: "Advanced", callback: { [weak self] in
